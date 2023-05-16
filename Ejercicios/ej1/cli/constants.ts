dc264d3da43f5ec9fce776d0e91ff69dd57879ba
Fransr24 <pereirafrancisco240501@gmail.com>
2023-05-16 11:53:36 -0300

    delete private key

--- Ejercicios/ej1/cli/constants.ts
+++ Ejercicios/ej1/cli/constants.ts
@@ -1,6 +1,6 @@
 import * as web3 from '@solana/web3.js';
 
-export const SECRET = [185,219,159,223,144,225,105,130,72,33,63,5,213,221,87,72,0,234,120,53,245,188,29,155,149,16,98,52,189,180,137,232,139,143,52,203,13,244,108,231,211,79,171,244,168,56,249,162,209,110,126,64,165,136,82,31,44,74,168,206,56,9,248,231];
+export const SECRET = [];
 // your local private test-key. Try: $cat ~/.config/solana/id.json
 export const PROGRAM_ID = 'CTmwKPss8TAAa6k3DG2351QtQPyGUUFWEmZXDUv3x72C'; // Whatever returned from solana deploy
 export const KEY: Uint8Array = Uint8Array.from(SECRET);
